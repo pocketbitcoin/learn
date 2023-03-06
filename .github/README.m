b74def717e349cd7d2752416e0f37752ec1d877c
Daniel Broll <98091744+danielbroll@users.noreply.github.com>
2023-03-06 12:52:19 +0100

    Create README.m

---
+++ .github/README.m
@@ -0,0 +1,9 @@
+# Pocket learn
+- how to contribute guide
+- what type of reward to expect per article
+- link to github issues (label with good first issue)
+
+# Articles
+- [set up trezor](./trezor/set-up-trezor.mdx)
+- [set up trezor manual](./trezor/set-up-trezor-manual.mdx)
+- ...
